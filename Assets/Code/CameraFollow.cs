1bd1223b9ecd29722651cd5fd7e8906937249355
Ben Clevenger <benhcleve@gmail.com>
2021-04-22 17:39:33 -0400

    Added mobile and desktop camera controls

---
+++ Assets/Code/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTransform;
+
+    void Update()
+    {
+        transform.position = followTransform.position;
+    }
+}
---
+++ Assets/Code/CameraOrbit.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrbit : MonoBehaviour
+{
+
+    protected Transform _XForm_Camera;
+    protected Transform _XForm_Parent;
+
+    protected Vector3 _LocalRotation;
+    protected float _CameraDistance = 10f;
+
+    public float OrbitSensitivity = .2f;
+    public float ZoomSensitivity = 2f;
+    public float OrbitDampening = 10f;
+    public float ZoomDampening = 6f;
+
+    public bool CameraDisabled = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this._XForm_Camera = this.transform;
+        this._XForm_Parent = this.transform.parent;
+    }
+
+
+    void LateUpdate()
+    {
+        CameraRotation();
+        CameraZoom();
+
+        //Actual Camera Rig Transformations
+        Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
+        this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
+
+        if (this._XForm_Camera.localPosition.z != this._CameraDistance * -1f)
+        {
+            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ZoomDampening));
+        }
+    }
+
+    void CameraRotation()
+    {
+        //DESKTOP
+#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            _LocalRotation.x += Input.GetAxis("Mouse X") * (OrbitSensitivity * 5); //Multiply for desktop;
+            _LocalRotation.y -= Input.GetAxis("Mouse Y") * (OrbitSensitivity * 5); //Multiply for desktop;
+
+            //Clamp the y Rotation to horizon and not flipping over at the top
+            if (_LocalRotation.y < 0f)
+                _LocalRotation.y = 0f;
+            else if (_LocalRotation.y > 90f)
+                _LocalRotation.y = 90f;
+        }
+#endif
+        //MOBILE
+
+#if UNITY_IOS || UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            _LocalRotation.x += Input.touches[0].deltaPosition.x * OrbitSensitivity;
+            _LocalRotation.y -= Input.touches[0].deltaPosition.y * OrbitSensitivity;
+
+            //Clamp the y Rotation to horizon and not flipping over at the top
+            if (_LocalRotation.y < 0f)
+                _LocalRotation.y = 0f;
+            else if (_LocalRotation.y > 90f)
+                _LocalRotation.y = 90f;
+        }
+#endif
+
+    }
+
+
+
+    void CameraZoom()
+    {
+        //Zooming Input from our Mouse Scroll Wheel
+#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            float zoomAmount = Input.GetAxis("Mouse ScrollWheel") * (ZoomSensitivity * 5); //Multiply for desktop
+
+            zoomAmount *= (this._CameraDistance * 0.3f);
+
+            this._CameraDistance += zoomAmount * -1f;
+
+            this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 100f);
+
+
+        }
+#endif
+
+#if UNITY_IOS || UNITY_ANDROID
+        if (Input.touchCount >= 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = (currentMagnitude - prevMagnitude) / 10;
+
+            float zoomAmount = difference * ZoomSensitivity;
+
+            zoomAmount *= (this._CameraDistance * 0.3f);
+
+            this._CameraDistance += zoomAmount * -1f;
+
+            this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 15f);
+        }
+    }
+#endif
+
+
+}
