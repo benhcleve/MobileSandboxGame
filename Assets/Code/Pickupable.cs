1173cb62fd734d65ffb713f07f26d893074e78f3
Ben Clevenger <benhcleve@gmail.com>
2021-06-12 01:58:23 -0400

    Oak Logs are now a collectable item

---
+++ Assets/Code/Pickupable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    public Item item;
+    public bool playerMagnet;
+
+    void Update() => Pickup();
+
+    void Pickup()
+    {
+        float distFromPlayer = Vector3.Distance(PlayerMovement.instance.transform.position, transform.position);
+        if (distFromPlayer < 3)
+            if (!PlayerInventory.instance.isInventoryFull())
+            {
+                if (playerMagnet && !GetComponent<Rigidbody>().isKinematic)
+                    transform.position = Vector3.MoveTowards(transform.position, PlayerMovement.instance.transform.position, 5 * Time.deltaTime);
+                if (distFromPlayer < .5f)
+                    PlayerInventory.instance.AddToInventory(item, gameObject);
+            }
+    }
+}
