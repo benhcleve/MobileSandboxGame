b87d3c5016039c75473d40c81c3156225d982ab2
Ben Clevenger <benhcleve@gmail.com>
2021-07-12 12:00:44 -0400

    Fishing Mechanics - Can now cast

--- Assets/Code/Tools/FishingRod.cs
+++ Assets/Code/Tools/FishingRod.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,13 +13,16 @@ public class FishingRod : MonoBehaviour
     public SpriteRenderer castTargetSprite;
     public LayerMask waterLayer;
     public GameObject bobber;
+    FishingLine fishingLine;
     bool canCast;
+    public GameObject castUI;
 
     private void OnEnable()
     {
         CameraManager.instance.state = CameraManager.State.Behind;
         CameraManager.instance.behindOffset = new Vector3(0.75f, 1.35f, 1.75f);
         UIManager.instance.uiState = UIManager.UIState.Default_NoMovement;
+        fishingLine = GetComponent<FishingLine>();
         powerSlider.maxValue = castPower; //Sets the power of the rod to the slider
         castTarget.SetActive(true);
     }
@@ -56,15 +60,46 @@ public class FishingRod : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(castTarget.transform.position, -castTarget.transform.up, out hit, 100f, waterLayer))
             {
-                castTarget.transform.position = hit.point + new Vector3(0, 1, 0);
+                castTarget.transform.position = hit.point + new Vector3(0, 0.2f, 0);
                 castTargetSprite.color = Color.green;
                 canCast = true;
             }
         }
     }
 
+    public static Vector3 Parabola(Vector3 start, Vector3 end, float height, float t)
+    {
+        Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
+
+        var mid = Vector3.Lerp(start, end, t);
+
+        return new Vector3(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t), mid.z);
+    }
+
     public void CastRod()
     {
+        if (canCast)
+        {
+            castUI.SetActive(false);
+            bobber.transform.parent = null;
+            bobber.transform.eulerAngles = Vector3.zero;
+            StartCoroutine(CastRodCo());
+        }
+    }
+
+    IEnumerator CastRodCo()
+    {
+        float castTime = 0;
+        Vector3 targetPos = castTarget.transform.position;
+        castTarget.SetActive(false);
+
+        while (castTime < 1f)
+        {
+            castTime += Time.deltaTime;
+            bobber.transform.position = Parabola(fishingLine.poleEnd.position, targetPos, 1, castTime);
+            yield return new WaitForEndOfFrame();
+        }
+        fishingLine.tension = -.1f;
 
     }
 }
