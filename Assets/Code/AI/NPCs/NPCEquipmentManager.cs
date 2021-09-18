2aa22961f01b02f8fbfeee30d70cb841e36b5de3
Ben Clevenger <benhcleve@gmail.com>
2021-09-18 15:10:16 -0400

    Created NPC Equipment Script. Added Comb over hair and polo shirt for Frank

---
+++ Assets/Code/AI/NPCs/NPCEquipmentManager.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NPCEquipmentManager : MonoBehaviour
+{
+    public GameObject avatar; //Drag human model to this slot, the whole model + Armature
+    private Stitcher stitcher;
+    public GameObject headWorn;
+    public GameObject facialHairWorn;
+    public GameObject chestWorn;
+    public GameObject legsWorn;
+    public GameObject feetWorn;
+    public GameObject backWorn;
+    public Transform rightHand;
+    public GameObject equippedItem;
+    public ItemSlot equippedItemSlot;
+
+    public void Awake()
+    {
+        stitcher = new Stitcher();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnSavedOutfit());
+    }
+
+    IEnumerator SpawnSavedOutfit()
+    {
+        StartCoroutine(Wear(2004, "Head", null, new Color32(50, 40, 30, 0)));
+        yield return new WaitForSeconds(0.01f);
+        StartCoroutine(Wear(2005, "Facial Hair", null, new Color32(50, 40, 30, 0)));
+        yield return new WaitForSeconds(0.01f);
+        StartCoroutine(Wear(2002, "Legs", null, new Color32(255, 255, 255, 0)));
+        yield return new WaitForSeconds(0.01f);
+        StartCoroutine(Wear(2006, "Chest", null, new Color32(110, 20, 20, 0)));
+    }
+
+    public void RemoveWorn(string apparelType)
+    {
+        switch (apparelType)
+        {
+            case "Head":
+                if (headWorn == null)
+                    return;
+                Destroy(headWorn);
+                headWorn = null;
+                break;
+            case "Facial Hair":
+                if (facialHairWorn == null)
+                    return;
+                Destroy(facialHairWorn);
+                facialHairWorn = null;
+                break;
+            case "Chest":
+                if (chestWorn == null)
+                    return;
+                Destroy(chestWorn);
+                chestWorn = null;
+                break;
+            case "Legs":
+                if (legsWorn == null)
+                    return;
+                Destroy(legsWorn);
+                legsWorn = null;
+                break;
+            case "Feet":
+                if (feetWorn == null)
+                    return;
+                Destroy(feetWorn);
+                feetWorn = null;
+                break;
+            case "Back":
+                if (backWorn == null)
+                    return;
+                Destroy(backWorn);
+                backWorn = null;
+                break;
+        }
+    }
+
+    public IEnumerator Wear(int apparelID, string apparelType, Texture texture, Color32 color)
+    {
+        Item[] items = Resources.LoadAll<ItemApparel>("Items/Apparel").ToArray();
+        foreach (Item i in items)
+            if (i.ID == apparelID)
+            {
+                GameObject clothing = Instantiate(i.prefab);
+
+                switch (apparelType)
+                {
+                    case "Head":
+                        headWorn = stitcher.Stitch(clothing, avatar);
+                        break;
+                    case "Facial Hair":
+                        headWorn = stitcher.Stitch(clothing, avatar);
+                        break;
+                    case "Chest":
+                        chestWorn = stitcher.Stitch(clothing, avatar);
+                        break;
+                    case "Legs":
+                        legsWorn = stitcher.Stitch(clothing, avatar);
+                        break;
+                    case "Feet":
+                        feetWorn = stitcher.Stitch(clothing, avatar);
+                        break;
+                    case "Back":
+                        backWorn = stitcher.Stitch(clothing, avatar);
+                        break;
+                }
+
+                yield return new WaitForSeconds(0.01f);
+
+                var clothingMesh = clothing.transform.GetChild(0).gameObject;
+
+                clothingMesh.GetComponent<SkinnedMeshRenderer>().material.SetColor("_MainColor", color);
+                if (texture != null)
+                    clothingMesh.GetComponent<SkinnedMeshRenderer>().material.SetTexture("_MainTex", texture);
+            }
+    }
+
+
+    public void EquipToolToHand(Item item)
+    {
+        equippedItemSlot = item.currentSlot;
+
+        //Remove if another item is already equipped
+        if (equippedItem != null)
+            Destroy(equippedItem);
+
+        equippedItem = Instantiate(item.prefab, rightHand.position, rightHand.rotation);
+        equippedItem.name = item.prefab.name; //Removes '(clone)' from name.
+        equippedItem.transform.parent = rightHand;
+    }
+
+    public void UnequipTool()
+    {
+        equippedItemSlot = null;
+        if (equippedItem != null)
+            Destroy(equippedItem);
+        equippedItem = null;
+    }
+}
